c2885f3072a091225705f2f6b3f9a1a76fba6788
thala254 <thalamine@gmail.com>
2023-01-18 20:29:58 +0300

    added advanced task file

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+web module
+"""
+import requests
+from redis import Redis
+rc = Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """
+    get a page and cache value
+    """
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
